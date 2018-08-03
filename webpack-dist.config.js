53dd62c401b8433602c4a996453c98af972a70d5
molang <ximolang@yeah.net>
2018-08-03 09:34:48 +0800

    ..

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -103,7 +103,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://resume.wuhaolin.cn' // 这里注意改成你的在线简历的网站
+        'http://resume.txliang.com' // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
